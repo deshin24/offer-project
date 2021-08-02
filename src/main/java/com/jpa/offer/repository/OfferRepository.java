ca11d6e480b1f2e2c4d49d90d8c31274ba6fe6c3
daeun-shin <deshin@snack24h.com>
2021-08-02 14:20:01 +0900

    OfferRepository 생성

---
+++ src/main/java/com/jpa/offer/repository/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.jpa.offer.repository;
+
+import com.jpa.offer.entity.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+}

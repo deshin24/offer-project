871eb9dfb1a046cfc69842508478daa0595fb0e6
daeun-shin <deshin@snack24h.com>
2021-08-02 09:37:49 +0900

    File 엔티티 생성과 test case 생성

---
+++ src/main/java/com/jpa/offer/entity/File.java
@@ -0,0 +1,45 @@
+package com.jpa.offer.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class File {
+
+    // 파일 pk
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private Long id;
+
+    // 파일명
+    @Column(name = "file_title", length = 50, nullable = false)
+    private String title;
+
+    // 파일 경로
+    @Column(name = "file_path", columnDefinition = "TEXT")
+    private String path;
+
+    // 연관관계 주인
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "offer_id" )
+    private Offer offer;
+
+    @Builder
+    public File(String title, String path, Offer offer) {
+        this.title = title;
+        this.path = path;
+        if(offer != null){ changeOffer(offer); }
+    }
+
+    private File changeOffer(Offer offer) {
+        this.offer = offer;
+        return this;
+    }
+}
---
+++ src/test/java/com/jpa/offer/entity/FileTest.java
@@ -0,0 +1,68 @@
+package com.jpa.offer.entity;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+class FileTest {
+
+    @Autowired EntityManager em;
+
+    @Test
+    public void fileTest(){
+        // given
+
+        // 회원 생성
+        User user = User.builder()
+                .name("회원1")
+                .email("user1@email.com")
+                .role(Role.USER)
+                .build();
+        em.persist(user);
+
+        // 제안 생성
+        Offer offer = Offer.builder()
+                .title("제안합니다")
+                .content("스낵24에 자사 제품 판매를 제안합니다.")
+                .serviceType(OfferServiceType.SNACK24)
+                .company("A회사")
+                .manager("김직원")
+                .phone("010-0000-0000")
+                .user(user)
+                .build();
+        em.persist(offer);
+
+        // 파일 생성
+        File file1 = File.builder()
+                .title("이미지1.jpg")
+                .path("이미지1 경로 ~ ")
+                .offer(offer)
+                .build();
+
+        File file2 = File.builder()
+                .title("이미지2.jpg")
+                .path("이미지2 경로 ~ ")
+                .offer(offer)
+                .build();
+        em.persist(file1);
+        em.persist(file2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<File> files = em.createQuery("select f from File f", File.class).getResultList();
+
+        // then
+        Assertions.assertThat(files.size()).isEqualTo(2);
+        // FetchType.LAZY 확인
+        Assertions.assertThat(files.get(0).getOffer().getTitle()).isEqualTo("제안합니다");
+    }
+}

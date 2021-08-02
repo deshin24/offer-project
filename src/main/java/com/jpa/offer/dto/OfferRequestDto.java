3f61026ff12c6882d43ab7f3e9785a8aa8817bd6
daeun-shin <deshin@snack24h.com>
2021-08-02 17:36:33 +0900

    OfferRequestDto 에 ApiModelProperty 설정 추가

--- src/main/java/com/jpa/offer/dto/OfferRequestDto.java
+++ src/main/java/com/jpa/offer/dto/OfferRequestDto.java
@@ -2,18 +2,26 @@ package com.jpa.offer.dto;
 
 import com.jpa.offer.entity.Offer;
 import com.jpa.offer.entity.OfferServiceType;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class OfferRequestDto {
 
+    @ApiModelProperty(value = "유저키", required = true, example = "1")
     private Long userId;
+    @ApiModelProperty(value = "제목", required = true, example = "제안합니다")
     private String title;
+    @ApiModelProperty(value = "내용", required = true, example = "자사와 ~~~ 제안합니다")
     private String content;
+    @ApiModelProperty(value = "서비스 타입", required = true, example = "SNACK24")
     private OfferServiceType serviceType;
+    @ApiModelProperty(value = "기업", required = true, example = "기업A")
     private String company;
+    @ApiModelProperty(value = "담당자", required = true, example = "담당자A")
     private String manager;
+    @ApiModelProperty(value = "전화번호", required = true, example = "010-1111-2222")
     private String phone;
 
     public Offer toEntity(){

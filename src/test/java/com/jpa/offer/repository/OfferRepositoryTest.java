93692aad3e513ea6293bf66d12ca89e6cb849267
daeun-shin <deshin@snack24h.com>
2021-08-04 10:34:34 +0900

    제안 목록 test case

--- src/test/java/com/jpa/offer/repository/OfferRepositoryTest.java
+++ src/test/java/com/jpa/offer/repository/OfferRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -55,5 +56,49 @@ public class OfferRepositoryTest {
         // then
         Assertions.assertThat(findOffer.getTitle()).isEqualTo(offer.getTitle());
         Assertions.assertThat(findOffer.getUser().getName()).isEqualTo(offer.getUser().getName());
+    }
+
+    /**
+     * 제안 리스트 테스트
+     */
+    @Test
+    public void offerListTest(){
+        // given
+        // 회원 생성
+        User user = User.builder()
+                .name("회원1")
+                .email("user1@email.com")
+                .role(Role.ADMIN)
+                .build();
+        userRepository.save(user);
+
+        // 제안 생성
+        Offer offer1 = Offer.builder()
+                .title("제안합니다1")
+                .content("스낵24에 자사 제품 판매를 제안합니다.")
+                .serviceType(OfferServiceType.SNACK24)
+                .companyName("A회사")
+                .managerName("김직원")
+                .phone("010-0000-0000")
+                .user(user)
+                .build();
+
+        Offer offer2 = Offer.builder()
+                .title("제안합니다2")
+                .content("생일24에 자사 제품 판매를 제안합니다.")
+                .serviceType(OfferServiceType.BIRTH24)
+                .companyName("A회사")
+                .managerName("김직원")
+                .phone("010-0000-0000")
+                .user(user)
+                .build();
+        offerRepository.save(offer1);
+        offerRepository.save(offer2);
+
+        // when
+        List<Offer> offers = offerRepository.findAll();
+
+        // then
+        Assertions.assertThat(offers.get(0).getServiceType()).isEqualTo(OfferServiceType.SNACK24);
     }
 }
